3080bc2f51f0b3851b914feb424800e363951a2e
QA2023 <aleksej-lizunov@mail.ru>
2023-06-15 17:51:15 +0300

    Initial commit

---
+++ src/test/java/com/example/demo5/MainPageTest.java
@@ -0,0 +1,48 @@
+package com.example.demo1;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.habr.com/");
+
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void changeLogTest() {
+        WebElement userIcon = driver.findElement(By.cssSelector("svg[data-test-id='menu-toggle-guest']"));
+        userIcon.click();
+
+        WebElement rulesLink = driver.findElement(By.xpath("//*[contains(text(), 'Как стать автором')]"));
+        rulesLink.click();
+
+        WebElement authorsLink = driver.findElement(By.xpath("//*[contains(text(), 'Авторы')]"));
+        authorsLink.click();
+    }
+}
